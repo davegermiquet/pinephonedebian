eea1b8f45ff56f547723cdcd877de994b2145196
Dave Germiquet <5960530-davegermiquet@users.noreply.gitlab.com>
2020-08-21 00:23:30 -0400

    Get rid of uncessary link

--- createImageSecond.sh
+++ createImageSecond.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Setting up serial
-echo "T0:23:respawn:/sbin/getty -L ttyS0 115200 vt100" > /arm64fs_debian/etc/inittab
+echo "T0:23:respawn:/sbin/getty -L ttyS0 115200 vt100" > /media/root/etc/inittab
 mount proc /media/root/proc -t proc
 mount devpts /media/root/dev/pts -t devpts
 
 cp /usr/bin/qemu-aarch64-static /media/root/usr/bin/
-chroot /arm64fs_debian sh -c "cd /dev; mknod -m 666 ttyS0 c 4 64"
-cp /etc/hosts /arm64fs_debian/etc/
+chroot /media/root sh -c "cd /dev; mknod -m 666 ttyS0 c 4 64"
+cp /etc/hosts /media/root/etc/
 
 # Our proxy is apt-cacher-ng, which can't handle SSL connections
 unset http_proxy
 
 # Allow non-free components
-echo "deb http://deb.debian.org/debian testing main contrib non-free"  > /arm64fs_debian/etc/apt/sources.list
-echo "deb https://repo.mobian-project.org mobian main non-free" >> /arm64fs_debian/etc/apt/sources.list
-echo APT::Default-Release \"testing\"\; > /arm64fs_debian/etc/apt/apt.conf.d/99defaultrelease
+echo "deb http://deb.debian.org/debian testing main contrib non-free"  > /media/root/etc/apt/sources.list
+echo "deb https://repo.mobian-project.org mobian main non-free" >> /media/root/etc/apt/sources.list
+echo APT::Default-Release \"testing\"\; > /media/root/etc/apt/apt.conf.d/99defaultrelease
 
-wget -O - https://repo.mobian-project.org/mobian.gpg.key > /arm64fs_debian/tmp/mobian.gpg.key
-chroot /arm64fs_debian apt-key add /tmp/mobian.gpg.key
+wget -O - https://repo.mobian-project.org/mobian.gpg.key > /media/root/tmp/mobian.gpg.key
+chroot /media/root apt-key add /tmp/mobian.gpg.key
 
 # Install optional packages
 
-chroot /arm64fs_debian  sh -c "/usr/bin/apt-get update"
-chroot /arm64fs_debian  sh -c "/usr/bin/apt-get install apt-utils"
-chroot /arm64fs_debian  sh -c "/usr/bin/apt-get -y install ca-certificates"
-chroot /arm64fs_debian sh -c "/usr/bin/apt-get install  -y --no-install-recommends gnupg2"
-chroot /arm64fs_debian /usr/bin/apt-get -y upgrade
-chroot /arm64fs_debian /usr/bin/apt-get install -y libpam-cap
-chroot /arm64fs_debian /usr/bin/apt-get install -y --no-install-recommends alsa-ucm-conf \
+chroot /media/root /usr/bin/apt-get update
+chroot /media/root  /usr/bin/apt-get install apt-utils
+chroot /media/root /usr/bin/apt-get -y install ca-certificates
+chroot /media/root /usr/bin/apt-get install  -y --no-install-recommends gnupg2
+chroot /media/root /usr/bin/apt-get -y upgrade
+chroot /media/root /usr/bin/apt-get install -y libpam-cap
+chroot /media/root /usr/bin/apt-get install -y --no-install-recommends alsa-ucm-conf \
   alsa-utils \
   busybox \
   ca-certificates \
@@ -116,57 +116,57 @@ chroot /arm64fs_debian /usr/bin/apt-get install -y --no-install-recommends alsa-
 BOOTUID=$(blkid -s UUID -o value /build/recovery-pinephone-loop0p1 | tr '\n' ' ')
 ROOTUID=$(blkid -s UUID -o value /build/recovery-pinephone-loop0p2 | tr '\n' ' ')
 
-echo "UUID=${ROOTUID}     /       f2fs   defaults       0       0" > /arm64fs_debian/etc/fstab
-echo "UUID=${BOOTUID}     /boot   ext2   defaults      0       0" >> /arm64fs_debian/etc/fstab
+echo "UUID=${ROOTUID}     /       f2fs   defaults       0       0" > /media/root/etc/fstab
+echo "UUID=${BOOTUID}     /boot   ext2   defaults      0       0" >> /media/root/etc/fstab
 
 # Setup hostname
-echo debian > /arm64fs_debian/etc/hostname
+echo debian > /media/root/etc/hostname
 
 # Generate locales (only en_US.UTF-8 for now)
-chroot /arm64fs_debian echo "LANG=en_US.UTF-8" > /etc/default/locale.gen
-chroot /arm64fs_debian echo "LANG=en_US.UTF-8" > /etc/default/locale
-chroot /arm64fs_debian local-gen
+chroot /media/root echo "LANG=en_US.UTF-8" > /etc/default/locale.gen
+chroot /media/root echo "LANG=en_US.UTF-8" > /etc/default/locale
+chroot /media/root local-gen
 
-chroot /arm64fs_debian /usr/bin/plymouth-set-default-theme solar
+chroot /media/root /usr/bin/plymouth-set-default-theme solar
 
 # Load phosh on startup if package is installed
-chroot /arm64fs_debian  /usr/bin/systemctl enable phosh.service
+chroot /media/root /usr/bin/systemctl enable phosh.service
 
-rm -rf  /arm64fs_debian/lib/firmware
-rsync -avh /build/firmware /arm64fs_debian/lib
+rm -rf  /media/root/lib/firmware
+rsync -avh /build/firmware /media/root/lib
 export USERNAME=debian
 export PASSWORD=1234
 
-chroot /arm64fs_debian adduser --gecos $USERNAME --disabled-password --shell /bin/bash $USERNAME --uid 1000
+chroot /media/root adduser --gecos $USERNAME --disabled-password --shell /bin/bash $USERNAME --uid 1000
 
 # Needed for hardware access rights
-chroot /arm64fs_debian adduser $USERNAME video
-chroot /arm64fs_debian adduser $USERNAME render
-chroot /arm64fs_debian adduser $USERNAME audio
-chroot /arm64fs_debian adduser $USERNAME bluetooth
-chroot /arm64fs_debian adduser $USERNAME plugdev
-chroot /arm64fs_debian adduser $USERNAME dialout
+chroot /media/root adduser $USERNAME video
+chroot /media/root adduser $USERNAME render
+chroot /media/root adduser $USERNAME audio
+chroot /media/root adduser $USERNAME bluetooth
+chroot /media/root adduser $USERNAME plugdev
+chroot /media/root adduser $USERNAME dialout
 
-chroot /arm64fs_debian sh -c 'echo "$USERNAME:$PASSWORD" | chpasswd'
-chroot /arm64fs_debian sh -c 'echo "root:root" | chpasswd'
+chroot /media/root sh -c 'echo "$USERNAME:$PASSWORD" | chpasswd'
+chroot /media/root sh -c 'echo "root:root" | chpasswd'
 
 # Remove apt packages which are no longer unnecessary and delete
 # downloaded packages
-chroot /arm64fs_debian update-locale
-chroot /arm64fs_debian apt -y autoremove --purge
-chroot /arm64fs_debian apt clean
+chroot /media/root update-locale
+chroot /media/root apt -y autoremove --purge
+chroot /media/root apt clean
 
 # Remove SSH keys and machine ID so they get generated on first boot
-chroot /arm64fs_debian rm -f /etc/ssh/ssh_host_* \
+chroot /media/root rm -f /etc/ssh/ssh_host_* \
       /etc/machine-id
 
 # Disable getty on tty1, as we won't connect in console mode anyway
-chroot /arm64fs_debian systemctl disable getty@.service
+chroot /media/root systemctl disable getty@.service
 
 # FIXME: these are automatically installed on first boot, and block
 # the system startup for over 1 minute! Find out why this happens and
 # avoid this nasty hack
-chroot /arm64fs_debian rm -f /lib/systemd/system/wpa_supplicant@.service \
+chroot /media/root rm -f /lib/systemd/system/wpa_supplicant@.service \
       /lib/systemd/system/wpa_supplicant-wired@.service \
       /lib/systemd/system/wpa_supplicant-nl80211@.service
 cd /
