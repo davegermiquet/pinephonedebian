5dd9f0b5d06d6e803962d54fedbc7a507aeb9fa3
Dave Germiquet <5960530-davegermiquet@users.noreply.gitlab.com>
2020-08-27 10:01:41 -0400

    Reorganize step process

--- kdebuildscript.sh
+++ kdebuildscript.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-cd /build/kdesrc-build
-./kdesrc-build --initial-setup
-source ~/.bashrc
+
 export CC=/usr/bin/aarch64-linux-gnu-gcc
 export CXX=/usr/bin/aarch64-linux-gnu-g++
+export Qt5_DIR=/build/qt5
+
+cd /build/qt5
+mkdir -p /build/qt5build/sysroot
+./configure -release -opengl es2 \
+ CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-\
+ -sysroot /build/qt5build/sysroot -opensource -confirm-license -skip qtwayland -skip qtlocation -skip qtscript -make libs \
+  -prefix /usr/local/qt5 -no-use-gold-linker -v -no-gbm
+
+
+./kdesrc-build --initial-setup
+source ~/.bashrc
+
+cd /build/kdesrc-build
+
 
 ./kdesrc-build plasma-nano plasma-phone-components plasma-settings plasma-camera marble koko vvave okular plasma-angelfish mtp-server kaidan peruse calindori index-fm maui-pix qrca keysmith --include-dependencies
 
