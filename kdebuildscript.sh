696be9d8fe0d9bfc8dbcc0803595174ff2cf07ee
Dave Germiquet <5960530-davegermiquet@users.noreply.gitlab.com>
2020-08-27 12:15:41 -0400

    add variable

--- kdebuildscript.sh
+++ kdebuildscript.sh
@@ -3,14 +3,15 @@
 export CC=/usr/bin/aarch64-linux-gnu-gcc
 export CXX=/usr/bin/aarch64-linux-gnu-g++
 export Qt5_DIR=/build/qt5
-
+export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-
 cd /build/qt5
+
 mkdir -p /build/qt5build/sysroot
 ./configure -release -opengl es2 \
- CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-\
  -sysroot /build/qt5build/sysroot -opensource -confirm-license -skip qtwayland -skip qtlocation -skip qtscript -make libs \
   -prefix /usr/local/qt5 -no-use-gold-linker -v -no-gbm
 
+cd /build/kdesrc-build
 
 ./kdesrc-build --initial-setup
 source ~/.bashrc
