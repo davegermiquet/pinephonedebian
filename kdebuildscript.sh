7ccc17a9530d14374d0356c6a2774420e69d5b20
Dave Germiquet <5960530-davegermiquet@users.noreply.gitlab.com>
2020-08-26 21:35:12 -0400

    Fix things

--- kdebuildscript.sh
+++ kdebuildscript.sh
@@ -3,7 +3,7 @@ cd /build/kdesrc-build
 ./kdesrc-build --initial-setup
 source ~/.bashrc
 export CC=/usr/bin/aarch64-linux-gnu-gcc
-export  CXX=/usr/bin/aarch64-linux-gnu-g++
+export CXX=/usr/bin/aarch64-linux-gnu-g++
 
 ./kdesrc-build plasma-nano plasma-phone-components plasma-settings plasma-camera marble koko vvave okular plasma-angelfish plasma-samegame mtp-server kaidan peruse calindori index-fm maui-pix qrca keysmith --include-dependencies
 
