a033d3009aa1ba09e144dcc976742452c5a3d5d6
Dave Germiquet <5960530-davegermiquet@users.noreply.gitlab.com>
2020-09-24 07:41:43 -0400

    Make install

--- scripts/qt5Build.sh
+++ scripts/qt5Build.sh
@@ -25,6 +25,6 @@ function setup_distcc() {
     export PATH=\"/usr/lib/distcc/:\$PATH\"
   fi
 }" > /media/fakeinstallroot/build/addFunction.tmp
-chroot /media/fakeinstallroot /usr/bin/bash -c "source /build/addFunction.tmp;setup_distcc;which gcc;cd /build/qt5/;./configure -prefix /usr -opensource -nomake tests && make -j4 &&  touch /build/qt5build.txt"
+chroot /media/fakeinstallroot /usr/bin/bash -c "source /build/addFunction.tmp;setup_distcc;which gcc;cd /build/qt5/;./configure -prefix /usr -opensource -nomake tests && make -j4 && make install  &&  touch /build/qt5build.txt"
 
 
