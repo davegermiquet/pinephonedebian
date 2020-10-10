67ab614f590fab2fbce5922bcc05ce521bc175b1
Dave Germiquet <5960530-davegermiquet@users.noreply.gitlab.com>
2020-10-10 01:51:31 -0400

    fix things

--- scripts/createKdePlasmaDependBuild.sh
+++ scripts/createKdePlasmaDependBuild.sh
@@ -13,5 +13,5 @@ chroot /media/fakeinstallroot /usr/bin/apt-get -y install autoconf baloo-kf5-dev
 chroot /media/fakeinstallroot /usr/bin/apt-get -y install flex bison gperf libicu-dev libxslt-dev ruby  libical-dev distcc ccache distcc-pump libxcb-composite0-dev xcb  libxcb-icccm4-dev
 chroot /media/fakeinstallroot /usr/bin/apt-get -y install libfontconfig1-dev libfreetype6-dev libx11-dev libx11-xcb-dev
 chroot /media/fakeinstallroot /usr/bin/apt-get -y install libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev
-chroot /media/fakeinstallroot /usr/bin/apt-get -y install libxcb-glx0-dev libxcb-keysyms1-dev  libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxcd-xinerama-dev libxkbcommon-dev libxkbcommon-x11-dev
+chroot /media/fakeinstallroot /usr/bin/apt-get -y install libxcb-glx0-dev libxcb-keysyms1-dev  libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxcd-xinerama-dev libxkbcommon-dev libxkbcommon-x11-dev libxext
 chroot /media/fakeinstallroot /usr/bin/apt-get -y upgrade
